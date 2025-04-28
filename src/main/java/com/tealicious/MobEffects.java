ce6f1185e9958bdb18cd71788bfbec273f623024
Shaun <shaunjullens@gmail.com>
2025-04-27 23:11:52 -0700

    Add reduced movement speed and attack speed

--- src/main/java/com/tealicious/MobEffects.java
+++ src/main/java/com/tealicious/MobEffects.java
@@ -3,7 +3,10 @@ package com.tealicious;
 import com.tealicious.effect.SleepyEffect;
 import com.tealicious.effect.TNTeaEffect;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -12,5 +15,16 @@ public class MobEffects {
 
     public static final DeferredHolder<MobEffect, MobEffect> EXPLOSION = REGISTRY.register("explosion", () -> new TNTeaEffect());
 
-    public static final DeferredHolder<MobEffect, MobEffect> SLEEPY = REGISTRY.register("sleepy", SleepyEffect::new);
+    public static final DeferredHolder<MobEffect, MobEffect> SLEEPY = REGISTRY.register(
+            "sleepy", () -> new SleepyEffect().addAttributeModifier(
+                    Attributes.MOVEMENT_SPEED,
+                    ResourceLocation.fromNamespaceAndPath("tealicious", "effect.movement_speed"),
+                    -0.25,
+                    AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
+            ).addAttributeModifier(
+                    Attributes.ATTACK_SPEED,
+                    ResourceLocation.fromNamespaceAndPath("tealicious", "effect.attack_speed"),
+                    -0.25,
+                    AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
+            ));
 }
--- src/main/java/com/tealicious/effect/SleepyEffect.java
+++ src/main/java/com/tealicious/effect/SleepyEffect.java
@@ -2,8 +2,12 @@ package com.tealicious.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.NotNull;
 
 public class SleepyEffect extends MobEffect {
     public SleepyEffect() {
-        super(MobEffectCategory.NEUTRAL, 0); }
+        super(MobEffectCategory.NEUTRAL, 0xFFFF00); }
 }
